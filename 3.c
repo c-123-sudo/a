a73fdae100ec22aceffa38ccbdb04b5a80da6c18
c-123-sudo <ruthwikbhargav@gmail.com>
2025-01-07 08:12:41 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -3,75 +3,80 @@
 #include <stdio.h>
 #include <string.h>
 
-void computeFailureFunction(char* pattern, int* failure) {
-    int i = 1, j = 0;
-    int m = strlen(pattern);
-    failure[0] = 0;
-    
-    while (i < m) {
-        if (pattern[i] == pattern[j]) {
-            failure[i] = j + 1;
-            i++;
-            j++;
-        }
-        else if (j > 0) {
-            j = failure[j - 1];
-        }
-        else {
-            failure[i] = 0;
-            i++;
-        }
-    }
+void computeFailureFunction(char *pattern, int *failure, int m);
+int KMPMatch(char *text, char *pattern);
+
+int main() {
+    char text[100], pattern[50];
+    int match;
+
+    printf("Enter the text: ");
+    scanf("%s", text);
+
+    printf("Enter the pattern: ");
+    scanf("%s", pattern);
+
+    match = KMPMatch(text, pattern);
+
+    if (match != -1)
+        printf("Pattern found at index %d\n", match);
+    else
+        printf("Pattern not found\n");
+
+    return 0;
 }
 
-void KMPSearch(char* text, char* pattern) {
-    int n = strlen(text);
-    int m = strlen(pattern);
-    int failure[m];
-    
-    computeFailureFunction(pattern, failure);
-    
-    printf("Failure Function: ");
-    for(int i = 0; i < m; i++) {
+// Function to compute the failure function of the pattern
+void computeFailureFunction(char *pattern, int *failure, int m) {
+    int i = 1, j = 0;
+    failure[0] = 0;
+
+    while (i < m) {
+        if (pattern[i] == pattern[j]) {
+            j++;
+            failure[i] = j;
+            i++;
+        } else {
+            if (j != 0) {
+                j = failure[j - 1];
+            } else {
+                failure[i] = 0;
+                i++;
+            }
+        }
+    }
+
+    printf("\nFailure Function: ");
+    for (i = 0; i < m; i++) {
         printf("%d ", failure[i]);
     }
     printf("\n");
-    
+}
+
+// Function to perform KMP pattern matching
+int KMPMatch(char *text, char *pattern) {
+    int n = strlen(text);
+    int m = strlen(pattern);
+    int failure[m];
     int i = 0, j = 0;
-    int found = 0;
-    
+
+    computeFailureFunction(pattern, failure, m);
+
     while (i < n) {
-        if (text[i] == pattern[j]) {
-            if (j == m - 1) {
-                printf("Pattern found at position: %d\n", i - j);
-                found = 1;
-                j = failure[j];
-            }
+        if (pattern[j] == text[i]) {
             i++;
             j++;
         }
-        else if (j > 0) {
-            j = failure[j - 1];
-        }
-        else {
-            i++;
-        }
-    }
-    
-    if (!found) {
-        printf("Pattern not found in text\n");
-    }
-}
 
-int main() {
-    char text[100], pattern[100];
-    
-    printf("Enter text: ");
-    scanf("%s", text);
-    printf("Enter pattern: ");
-    scanf("%s", pattern);
-    
-    KMPSearch(text, pattern);
-    
-    return 0;
+        if (j == m) {
+            return i - j; // Match found, return the starting index
+        } else if (i < n && pattern[j] != text[i]) {
+            if (j != 0)
+                j = failure[j - 1];
+            else
+                i++;
+        }
+    }
+
+    return -1; // Match not found
 }
